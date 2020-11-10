dd169721276d5a3e9a67a8b2466e0c7ed9cadafa
Sebastian Kopf <sebkopf>
2020-11-10 12:57:57 -0700

    add binder

---
+++ binder/install.R
@@ -0,0 +1,6 @@
+# dependencies
+install.packages(c("tidyverse", "latex2exp", "cowplot", "broom", "phangorn", "BiocManager"))
+BiocManager::install("ggtree")
+
+# Packages for knitting
+install.packages(c("rmarkdown", "caTools", "bitops"))
